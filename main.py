0c381afbb24691ab5fe9d036ad5ebf4e7f4958f9
Brent Westbrook <brentrwestbrook@gmail.com>
2021-11-30 17:08:48 -0600

    try to find conversion factor mistake

--- main.py
+++ main.py
@@ -43,12 +43,16 @@ vals = [0 if abs(x) < 1e-10 else x for x in vals]
 if crawford:
     conv = 2625.498413 * 1000 / (AVOGADRO * BOHR * BOHR * 1.66054e-27)
     for v in vals:
-        print(math.sqrt(v*conv)/(100*c*2*math.pi))
+        print("%.4f" % (math.sqrt(v*conv)/(100*c*2*math.pi)))
 # fcs in attojoules
 else:
     # 5.034e+3 from
     # http://laser.chem.olemiss.edu/~nhammer/constants.html
     # 1.02 is a magic number to match spectro output
-    vals = np.sqrt(vals) * 5.034e+3 * 1.02115
-    print(vals)
+    a = np.sqrt(vals)[-3:]
+    h = 6.62607015e-34
+    print(a*1e-21/(h*c))
+    print(a[2]/a[0])
+    # vals = np.sqrt(vals) * 5.034e+3 * 1.02115
+    # print(vals)
 
